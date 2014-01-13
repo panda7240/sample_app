9911ffe3da3da44f05a9bb1b972a29856ad91645
panda <siye1982@163.com>
2014-01-13 15:36:20 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+
+  def create
+    #等同于@user = User.new(name: "Foo Bar", email: "foo@invalid", password: "foo", password confirmation: "bar")
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
 
 
   def show
