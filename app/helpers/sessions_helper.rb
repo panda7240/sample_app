99aa0b725d252c02ee7aeb358e57ae9dce27faf7
panda <siye1982@163.com>
2014-01-14 10:09:00 +0800

    update sign-in-out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
   def sign_in(user)
     #cookies[:remember_token] = { value: user.remember_token, expires: 20.years.from_now.utc } cookies20年之后才失效
     #等同于下面一行代码
-    session[:remember_token] = user.remember_token
+    cookies[:remember_token] = user.remember_token
 
     #在视图中可以这样用<%= current_user.name %>
     self.current_user = user
@@ -17,29 +17,20 @@ module SessionsHelper
     @current_user = user
   end
 
+
+  def signed_in?
+    !current_user.nil?
+  end
+
   #当且仅当@current_user未定义时才进行赋值,避免重复查询数据库
   def current_user
-    if session[:remember_token] == nil
-      p '11111111111111111111111'
-      return nil
-    else
-      current_user = User.find_by_remember_token(session[:remember_token])
-      if current_user == nil
-        p '22222222222222222222222'
-        return nil
-      else
-        p '33333333333333333333333'
-        return current_user
-      end
-    end
-
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
 
   def sign_out
     self.current_user = nil
-    #cookies.delete(:remember_token)
-    session[:remember_token] = nil
+    cookies[:remember_token] = nil
   end
 
 end
